6e3e270a12c5461cd814e438dea33dc70d817503
Mert <mert1996comert@gmail.com>
2022-06-24 11:09:20 +0300

    Custom Plugin Test File

---
+++ deneme/src/main/java/com/mert/deneme/App.java
@@ -0,0 +1,13 @@
+package com.mert.deneme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
